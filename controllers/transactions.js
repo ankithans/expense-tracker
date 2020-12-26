da796d080e26ce7e1297b0169d776d3ef418e9db
Ankit Hans <ankithans1947@gmail.com>
2020-12-27 00:00:03 +0530

    feat: get transaction api

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,34 @@
+const Transaction = require("../models/transaction");
+
 // @desc    Get all transactions
 // @route   GET /api/v1/transactions
 // @access  Public
-exports.getTransactions = (req, res, next) => {
-  res.send("GET transactions");
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (err) {
+    res.send(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
 };
 
 // @desc    Add a transactions
 // @route   POST /api/v1/transactions
 // @access  Public
-exports.addTransactions = (req, res, next) => {
+exports.addTransactions = async (req, res, next) => {
   res.send("POST transactions");
 };
 
 // @desc    Delete all transactions
 // @route   DELETE /api/v1/transactions/:id
 // @access  Public
-exports.deleteTransactions = (req, res, next) => {
+exports.deleteTransactions = async (req, res, next) => {
   res.send("DELETE transactions");
 };
