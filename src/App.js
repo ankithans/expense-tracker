c9e5f9df615c80e2263eb27c1507c73ce41507f8
Ankit Hans <ankithans1947@gmail.com>
2020-12-26 21:15:31 +0530

    feat: add all css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className='container'>
+        <Balance />
+      </div>
+    </div>
+  );
 }
 
 export default App;
