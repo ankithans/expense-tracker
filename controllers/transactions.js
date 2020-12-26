ac0c95013863d8ddeb4c1feae89d460e53119b69
Ankit Hans <ankithans1947@gmail.com>
2020-12-27 00:17:13 +0530

    feat: add transaction and delete transaction api

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -12,7 +12,7 @@ exports.getTransactions = async (req, res, next) => {
       data: transactions,
     });
   } catch (err) {
-    res.send(500).json({
+    res.status(500).json({
       success: false,
       error: "Server Error",
     });
@@ -23,12 +23,52 @@ exports.getTransactions = async (req, res, next) => {
 // @route   POST /api/v1/transactions
 // @access  Public
 exports.addTransactions = async (req, res, next) => {
-  res.send("POST transactions");
+  try {
+    const { text, amount } = req.body;
+    const transaction = await Transaction.create(req.body);
+    return res.status(201).json({
+      success: true,
+      data: transaction,
+    });
+  } catch (err) {
+    console.log(err.message);
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors).map((val) => val.message);
+      return res.status(400).json({
+        success: false,
+        error: messages,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: "Server Error",
+      });
+    }
+  }
 };
 
 // @desc    Delete all transactions
 // @route   DELETE /api/v1/transactions/:id
 // @access  Public
 exports.deleteTransactions = async (req, res, next) => {
-  res.send("DELETE transactions");
+  try {
+    const transaction = await Transaction.findById(req.params.id);
+    if (!transaction) {
+      return res.status(404).json({
+        success: false,
+        error: "No transaction found",
+      });
+    } else {
+      await transaction.remove();
+      return res.status(200).json({
+        success: true,
+        data: {},
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
 };
