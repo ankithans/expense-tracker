6b4ae5cfe00de2151f14f5e24034d699b75e21c7
Ankit Hans <ankithans1947@gmail.com>
2020-12-27 10:04:57 +0530

    feat(*): serve react in production

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const colors = require("colors");
 const morgan = require("morgan");
 const connectDB = require("./services/db");
@@ -17,6 +18,13 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use("/api/v1/transactions", transactions);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(
